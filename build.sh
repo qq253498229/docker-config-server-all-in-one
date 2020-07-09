42b4eb8a5a85a0e90af0a973a978d43c78e55862
大胃王 <253498229@qq.com>
2020-07-09 17:46:05 +0800

    重写文档

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 rm -rf docs
-mkdir -p docs/images/ docs/picture/
+mkdir -p docs/images/
 cp -r source/css/images/ docs/images
-cp -r source/css/picture/ docs/picture
 asciidoctor ./source/cn/index.adoc -o ./docs/index.html
